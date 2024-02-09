7fdaa36ce22f3b65535a814d814e4b9ece90200b
Venkatesh3696 <venkateshpentakota888@gmail.com>
2024-02-09 12:57:48 +0530

    final commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+export class Home extends Component {
+  static propTypes = {};
+
+  render() {
+    return (
+      <div>
+        <h1>home</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/Login/index.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    );
+  }
+}
